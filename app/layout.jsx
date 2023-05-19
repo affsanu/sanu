14ae201998c86e51123aaabf51e625d6de693d09
Sanu Islam <sanuvipbd@gmail.com>
2023-05-19 23:15:27 +0600

    Footer Add

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from './components/Material';
 import TopNav from './components/layouts/TopNav';
+import Footer from './components/layouts/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider>
           <TopNav />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@ import { Button } from './components/Material';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-full flex-col items-center justify-between p-24">
       <h1>Welcome Home</h1>
-      <Button>Click Here</Button>
+
     </main>
   )
 }
