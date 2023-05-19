486e686e3fb24c20f5cd697e3285e8dda1e1893e
Sanu Islam <sanuvipbd@gmail.com>
2023-05-19 23:02:43 +0600

    Material

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Button } from './components/Material';
 
 export default function Home() {
   return (
-    <main className="prose w-full py-10 px-5 mx-auto">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome Home</h1>
+      <Button>Click Here</Button>
     </main>
   )
 }
