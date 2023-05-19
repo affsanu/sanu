dfd2b1fb4e098aea327a496d3f9c9b37911bac16
Sanu Islam <sanuvipbd@gmail.com>
2023-05-19 22:49:18 +0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="prose w-full py-10 px-5 mx-auto">
+      <h1>Welcome Home</h1>
+    </main>
+  )
+}
